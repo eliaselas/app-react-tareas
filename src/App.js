8723b9263b1c2a8d2e614cf4a8f3e3a74f02e0b9
elias <hernandezelas@gmail.com>
2022-10-18 17:50:45 -0600

    agregando hojas de estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import freecodeacamplogo from "./imagenes/freecodecamp-logo.png";
+import ListaTarea from "./componentes/ListaTarea";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={freecodeacamplogo}
+          alt="logo de freecodecamp"
+        />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis tareas</h1>
+        <ListaTarea />
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Tarea.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../hojas-de-estilo/Tarea.css";
+import { AiTwotoneDelete } from "react-icons/ai";
+function Tarea({ id, texto, completada, completarTarea, eliminarTarea }) {
+  return (
+    <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
+      <div className="tarea-texto" onClick={() => completarTarea(id)}>{texto}</div>
+      <div className="tarea-contenedor-iconos" onClick={() => eliminarTarea(id)}><AiTwotoneDelete  className='tarea-icono'/></div>
+    </div>
+  );
+}
+export default Tarea;
